31c68a78a33b12d4377137af5a41b53171d4c875
Eugene Whatley <ewhatley@thoughtworks.com>
2019-09-10 15:01:43 -0500

    Add ssh server profile

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+	package {'openssh-server':
+		ensure	=> present,
+	}
+	service { 'sshd':
+		ensure	=> 'running',
+		ensure	=> 'true',
+	}
+	ssh_authorized_key { 'root@master.puppet.vm':
+		ensure	=> present,
+		user	=> 'root',
+		type	=> 'ssh-rsa',
+		key	=> 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTj6SfAfef4oZSF73JokLKyn7r0L6Z0lZFGqsPcZuNe+CoG6bb+U1fRlaC1u9njvMEr8/orhves8ri7YG7VYKOI6SxruvI0feXqR5dY71HTXhMQqpqS56BUlNx2DWxDB1RB7QcQESYnwVxq+bQp7rAIc7sUZJftQzKR7VyLr44Ybw2eF7W70e/cEBOWDArKVTyjnULN1fE0TG3g6SDeSdZKtQUWHbs/2D6XzbTZXZM9neK0ZVcNvmFdeht16tBUpNPvkQ9OItEB9DZ1xRWP9L5kXwNn0E7AEtcprP0P1xv9DDmnmcnrTFdJUEKrYMaFF4eFO5CO36iSFl4ebKmyHEf',
+	}
+}
